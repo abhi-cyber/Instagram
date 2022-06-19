f7e6b94836475de5aab625181c885a4e53254870
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-19 20:37:28 +0530

    Contnet edited

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,24 @@
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import React, { useState } from "react";
 import SearchBox from "../components/SearchBox";
 import SearchContent from "../components/SearchContent";
+import Ant from "react-native-vector-icons/AntDesign";
+import { StatusBar } from "expo-status-bar";
 
 const Search = () => {
+  const [image, setImage] = useState(null);
+  const getData = (data) => {
+    setImage(data);
+  };
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
   return (
     <View
       style={{
@@ -13,10 +28,16 @@ const Search = () => {
         position: "relative",
       }}
     >
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <SearchBox />
-        <SearchContent />
+        <SearchContent data={getData} />
+        <TouchableOpacity
+          style={{ margin: 25, justifyContent: "center", alignItems: "center" }}
+        >
+          <Ant name="pluscircleo" style={{ fontSize: 40, opicity: 0.5 }} />
+        </TouchableOpacity>
       </ScrollView>
+      
     </View>
   );
 };
